e6c4bf052aa1c06dd79abc91f8ce4994765e287e
RohiniAlla <133994292+RohiniAlla@users.noreply.github.com>
2023-06-20 14:15:19 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,13 +2,12 @@ terraform {
   required_version = ">= 1.0"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source  = "registry.terraform.io/hashicorp/aws"
       version = ">= 4.59"
     }
   }
 }
 
 provider "aws" {
-  alias  = "us-west-2"
   region  = "us-west-2"
 }
